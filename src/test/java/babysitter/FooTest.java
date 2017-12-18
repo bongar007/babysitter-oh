3416545eeeb0cb43646c89c7e5c01dc69a170730
bongar007 <bongar007@gmail.com>
2017-12-17 22:47:55 -0500

    basic project setup

---
+++ src/test/java/babysitter/FooTest.java
@@ -0,0 +1,13 @@
+package babysitter;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FooTest {
+
+	@Test
+	public void shouldFoo() {
+		assertEquals("it foos", true, true);
+	}
+}
