9e759e547530bb0ef1f6f1480c4797091ec743c1
bongar007 <bongar007@gmail.com>
2018-02-06 15:09:43 -0500

    fixing calculations involving invalid hours

--- src/main/java/babysitter/BabySitter.java
+++ src/main/java/babysitter/BabySitter.java
@@ -34,12 +34,9 @@ public class BabySitter {
 
 	
 	int calculatePay() {
-		if(isValidWorkHours(startTime, endTime)) {
 		return hoursBeforeBedtime() * BEFORE_BEDTIME_RATE
 				+ hoursBetweenBedtimeAndMidnight() * BETWEEN_BEDTIME_AND_MIDNIGHT_RATE
 				+ hoursAfterMidnight() * AFTER_MIDNIGHT_RATE;
-		}
-		return 0;
 	}
 	
 
@@ -69,34 +66,35 @@ public class BabySitter {
 	}
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		try(Scanner sc = new Scanner(System.in)){ 
 		
-		System.out.println("Hello, I am your baby sitter for tonight!");
-		System.out.println();
-		System.out.println("My hours are between 5PM and 4AM");
-		System.out.println();
-		System.out.println("From 5Pm until your desired bedtime, I charge $12/hr");
-		System.out.println("From bedtime until 12Am, it's $8/hr");
-		System.out.println("And midnight until 4Am, it's $16/hr");
-		System.out.println();
-	
-		System.out.println("Please enter the start time (In 24 hour format please):");
-		int start = sc.nextInt();
-		System.out.println();
-		System.out.println("Please enter the end time (In 24 hour format please):");
-		int end = sc.nextInt();
-		System.out.println();
-		System.out.println("Please add the desired bedtime(In 24 hour format please):");
-		int bedtime = sc.nextInt();
-		System.out.println("Thank you!");
+			System.out.println("Hello, I am your baby sitter for tonight!");
+			System.out.println();
+			System.out.println("My hours are between 5PM and 4AM");
+			System.out.println();
+			System.out.println("From 5Pm until your desired bedtime, I charge $12/hr");
+			System.out.println("From bedtime until 12Am, it's $8/hr");
+			System.out.println("And midnight until 4Am, it's $16/hr");
+			System.out.println();
 		
-		BabySitter pay = new BabySitter(start, end, bedtime);
-		
-		if(pay.calculatePay() == 0) {
+			System.out.println("Please enter the start time (In 24 hour format please):");
+			int start = sc.nextInt();
+			System.out.println();
+			System.out.println("Please enter the end time (In 24 hour format please):");
+			int end = sc.nextInt();
+			System.out.println(); 
+			System.out.println("Please add the desired bedtime(In 24 hour format please):");
+			int bedtime = sc.nextInt();
+			System.out.println("Thank you!");
+			
+			
+			BabySitter pay = new BabySitter(start, end, bedtime);
+			
+			if(pay.isValidWorkHours(start, end) == true) {
+				System.out.println("Your payment for the night is: " + "$" + pay.calculatePay());
+			} else {
 			System.out.println("Sorry, can't work those hours. Business hours from 5PM - 4AM");
-		} else {
-			System.out.println("Your payment for the night is: " + "$" + pay.calculatePay());
+			}
 		}
-		
-	}
+	}	
 }
